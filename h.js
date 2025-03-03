225d2e042523a9f7f02b59c335275d58c1251d4c
Arul A <nareshlaesn170705@gmail.com>
2025-03-03 21:10:38 +0530

    First

---
+++ h.js
@@ -0,0 +1,6 @@
+// function method(){
+//     let cmval=Number(document.getElementById("input").value)
+//     let inchval=cmval/2.54
+//     let result=Document.getElementById("result")
+//     result.innerHTML=inchval.toFixed(2)
+// }
